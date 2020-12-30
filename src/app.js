ebc33291ba50c297b888d67fd37074448e69fd8e
robpwatkins <robpwatkins@gmail.com>
2020-12-30 15:42:16 -0600

    got thereeee

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h3>From src/app.js</h3>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
